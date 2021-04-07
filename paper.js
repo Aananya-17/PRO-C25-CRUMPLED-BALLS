b5e9138af6e1bf03e60ac45eabad9aefde2df65d
Aananya-17 <77152034+Aananya-17@users.noreply.github.com>
2021-04-07 09:44:34 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,13 @@
+class paperclass(){
+    var options={
+        isstatic: false,
+        retitution:0.3,
+        friction:0,
+        density:1.2
+    }
+    Image = loadImage(paper.png);
+
+    display(){
+        Image.display();
+    }
+}
